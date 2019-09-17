8994e4efed475f7f9c3c0f5c051d686559b1941d
minestarnl <bramkoene@gmail.com>
2019-09-17 12:52:26 +0200

    Add files via upload

---
+++ Matrix.bat
@@ -0,0 +1,12 @@
+@echo off
+color 0A
+echo Hacking PC and user data.....
+sleep 2
+echo Installing backdoor.....
+sleep 2
+
+:back
+sleep 0.005s
+echo %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+echo  %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random% %random%
+goto back
---
+++ rickroll.bat
@@ -0,0 +1,3 @@
+@echo off
+start iexplore.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ
+start microsoft-edge:https://www.youtube.com/watch?v=dQw4w9WgXcQ
